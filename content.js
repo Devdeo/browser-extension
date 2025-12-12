b397d92a5a49abf6c1ad860a407e0afdb78ba455
Devdeo <46339020+Devdeo@users.noreply.github.com>
2025-12-12 09:11:21 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 // content.js - injects inject.js into page context
-const s = document.createElement('script');
-s.src = chrome.runtime.getURL('inject.js');
-s.type = 'text/javascript';
-s.onload = function(){ this.remove(); };
-(document.head || document.documentElement).appendChild(s);
+const script = document.createElement('script');
+script.src = chrome.runtime.getURL('inject.js');
+script.type = 'text/javascript';
+script.onload = function(){ this.remove(); };
+(document.head || document.documentElement).appendChild(script);
